8569459ed74dbc27fccd67663657f7186e1f32f2
BuWH <wenhelog@gmail.com>
2018-03-09 23:41:16 +0800

    add file_process.cpp

---
+++ MiniWord/MiniWord/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniWord
+//
+//  Created by 温和 on 2018/3/9.
+//  Copyright © 2018年 温和. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ file_process.cpp
@@ -0,0 +1,9 @@
+//
+//  file_process.cpp
+//  MiniWord
+//
+//  Created by 温和 on 2018/3/9.
+//  Copyright © 2018年 温和. All rights reserved.
+//
+
+#include <stdio.h>
