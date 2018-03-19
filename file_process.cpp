52e8dbf9806469adb4f9b5c2f5041b0c87193c2c
BuWH <wenhelog@gmail.com>
2018-03-20 00:06:08 +0800

    add one function

--- file_process.cpp
+++ file_process.cpp
@@ -8,9 +8,17 @@ std::string FileProcess::read(){
     if(FileProcess::file_self.is_open()){
         std::string data;
         getline(file_self,data);
+        std::cout<<"length = "<<file_self.tellg()<<std::endl;
         return data;
     }
     else{
-        return "error: file not opened.\n";
+        exit(0);
     }
+}
+
+bool FileProcess::is_end(){
+    if(file_self.tellg() == -1)
+        return 1;
+    else
+        return 0;
 }
--- file_process.h
+++ file_process.h
@@ -8,8 +8,10 @@
 class FileProcess{
 public:
     std::string read();//用于读取文件，每使用一次，读取一行
-    std::fstream file_self;
     FileProcess(const std::string address);//打开文件流
+    bool is_end();//读取文件内容时，用于判断是否为结尾
+private:
+    std::fstream file_self;
 };
 
 #endif /* file_process_h */
