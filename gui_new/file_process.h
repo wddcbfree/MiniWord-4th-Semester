980d7dd678637267a6a35c6b61e845ce73a4c6cd
LiuSecone <liusicong1998@outlook.com>
2018-05-30 15:19:15 +0800

    creat VS project, and change code style

--- gui_new/file_process.h
+++ gui_new/file_process.h
@@ -15,34 +15,34 @@ class FileProcess : public QObject
 {
     Q_OBJECT
 public:
-    void init_memory(Text *memory){
+    void init_memory(Text *memory) {
         Memory = memory;
     }
-    QVector<QString> get_data(){
+    QVector<QString> get_data() {
         return Data;
     }
-    void pre_set_data(){
+    void pre_set_data() {
         Data.clear();
     }
-    void set_data(QString line){
+    void set_data(QString line) {
         Data.push_back(line);
     }
-    void clearData(){
+    void clearData() {
         Data.clear();
     }
-    bool is_create(){
+    bool is_create() {
         return CreateSignal;
     }
-    bool is_open(){
+    bool is_open() {
         return OpenSignal;
     }
-    bool is_edited(){
+    bool is_edited() {
         return EditedSignal;
     }
-    void set_edited(bool is_not){
+    void set_edited(bool is_not) {
         EditedSignal = is_not;
     }
-public slots:
+    public slots:
     void open_file(QString path);
     void create_file();
     void save_file(QString path);
